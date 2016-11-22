9b7b0e13c1c7c53e6db4bcc721c1182e861ccaaa
Maximilian Meister <mmeister@suse.de>
2016-11-22 10:14:40 +0100

    upgrade: add subcommand to cancel the upgrade

---
+++ spec/crowbar/client/command/upgrade/cancel_spec.rb
@@ -0,0 +1,31 @@
+#
+# Copyright 2016, SUSE Linux GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative "../../../../spec_helper"
+
+describe "Crowbar::Client::Command::Upgrade::Cancel" do
+  include_context "command_context"
+
+  it_behaves_like "a command class", true do
+    subject do
+      ::Crowbar::Client::Command::Upgrade::Cancel.new(
+        stdin,
+        stdout,
+        stderr
+      )
+    end
+  end
+end
